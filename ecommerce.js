4d7ac0861a9438e85c1e87a7a906d32a6b2b03fc
unknown <victorjames40v@gmail.com>
2023-06-03 00:38:15 +0100

    adding moile responsiveness

--- ecommerce.js
+++ ecommerce.js
@@ -8,6 +8,7 @@ const itemContainer = document.querySelector(".cartitem")
 const numberOfitem = document.querySelector(".no")
 const navBarCollection = document.querySelector(".btnCollection");
 const nike = document.querySelector(".nike")
+const mainBody=document.querySelector(".bodyMain")
 const adidas = document.querySelector(".adidas")
 const payMent = document.querySelector(".payment")
 const subTotalPrice=document.querySelector("#sub")
@@ -30,7 +31,8 @@ const products=[
         name:"adidas 3",
         picture:"assets/add2.png",
         price: "$200",
-        id:"pro2"
+        id: "pro2",
+        otherPart:["assets/add2.png","assets/add4.jpg","assets/Add7.jpg","assets/shoe1.png"]
     },
     {
         name:"adidas 3",
@@ -105,23 +107,37 @@ function Products(){
 }
 // display products base on brand
 function DisplayBrandItems(brand) {
-    
+    if (collection.style.display === "") {
+        collection.style.display = "grid",
+            homepage.style.display = "none";
+        cartItem.style.display = "";
+        product = ''
+        Products()
+    } else {
+        collection.style.display = "",
+        homepage.style.display = "";
+        cartItem.style.display = "";
+    }
     let filteredItem = products.filter(items => {
         return items.name.includes(brand)
     })
     product=""
     for(let i=0;i<filteredItem.length; i++){
-        product +=`<div class="pro-items">
-        <img src=${filteredItem[i].picture}>
-        <div class="about">
-            <p>${filteredItem[i].name}</p>
-            <span>${filteredItem[i].price}</span>
-        </div>
-        <button class="addCart">Add to cart</button>
-        </div>`  
+        product +=`<div class="pro-items" id=${products[i].id}>
+          <div class='pro'>
+                <img src=${products[i].picture}>
+                <div class="about">
+                <p>${products[i].name}</p>
+                <span>${products[i].price}</span>
+            </div>
+          </div>
+          <button class="addCart">Add to cart</button>
+          </div>`
         
         mainProduct.innerHTML = product;
         let addCart = document.querySelectorAll(".addCart")
+         let productItems = document.querySelectorAll(".pro")
+        Redirect(productItems)
         AddItemToCart(addCart)
     
     }  
@@ -162,12 +178,14 @@ function DisplayProducts() {
             collection.style.display = "grid",
                 homepage.style.display = "none";
             cartItem.style.display = "";
+            mainBody.style.display="none"
             product = ''
             Products()
         } else {
             collection.style.display = "",
             homepage.style.display = "flex";
             cartItem.style.display = "";
+            mainBody.style.display="none"
         }
     })
 }
@@ -185,20 +203,19 @@ function DisplayPayment(number,payment){
 function AddItemToCart(placeholder){
     placeholder.forEach(addCart =>{
         addCart.addEventListener("click", (e) => { 
-            numberOfItemOnChart += 1
-            numberofCart.innerHTML = numberOfItemOnChart;
-            numberOfitem.innerHTML = numberOfItemOnChart;
-           DisplayPayment(numberOfItemOnChart,payMent)
+            // numberOfItemOnChart += 1
+            // numberofCart.innerHTML = numberOfItemOnChart;
+            // numberOfitem.innerHTML = numberOfItemOnChart;
+           DisplayPayment(previousCartImage.length,payMent)
            let Target=e.target;
             parent = Target.parentElement;
             let itemName = parent.querySelector("p").innerHTML;
             itemPrice=parent.querySelector("span").innerHTML
             itemImage=parent.querySelector("img").src
-             
+            itemId = parent.id;
            
-                
            
-            let itemdetails = `<div class="itemsOnCart" id=>
+            let itemdetails = `<div class="itemsOnCart" id=${itemId}>
                 <div class="product">
                 <img src="${itemImage}" >
                 <h2>${itemName}</h2> 
@@ -217,15 +234,16 @@ function AddItemToCart(placeholder){
                 </div>
             </div>
             `
-            itemContainer.innerHTML += itemdetails;
                 
             // check if an item is already in the cart
-            //  if (previousCartImage.includes(itemImage)) {
-            //     null
-            // } else {
-            //      previousCartImage.push(itemImage)
-                 
-            // }
+             if (previousCartImage.includes(itemId)) {
+                null
+             } else {
+                 itemContainer.innerHTML += itemdetails;
+                 previousCartImage.push(itemId)
+                 numberofCart.innerHTML = previousCartImage.length;
+                numberOfitem.innerHTML = previousCartImage.length;
+            }
             
            
             let itemQuantity = document.querySelectorAll(".itemsOnCart")
@@ -284,26 +302,22 @@ function AddItemToCart(placeholder){
                     let removeBotton = itemQuantity[i].querySelectorAll(".btn2");
                     for (let i = 0; i < removeBotton.length; i++){
                         removeBotton[i].addEventListener("click", (e) => {
-                            numberOfItemOnChart -= 1
-                            numberofCart.innerHTML = numberOfItemOnChart;
-                            numberOfitem.innerHTML = numberOfItemOnChart;
-                            DisplayPayment(numberOfItemOnChart,payMent)
+                            DisplayPayment(previousCartImage.length,payMent)
                             let parent = e.target.parentElement.parentElement;
                             parent.remove()
                             let removeTotalPrice = parent.querySelector(".totalPrice").innerHTML
                             let removeTotalPrice2 = parseInt(removeTotalPrice.replace("$", ""))
                             let currentSubTotal = subTotalPrice.innerHTML;
-                            console.log(previousCartImage)
+                            const removedId=parent.id
 
                             // to remove image from the previous array;
 
-                            // let imageIndex = previousCartImage.indexOf(itemImage)
-                            // if (imageIndex !== -1) {
-                            //     previousCartImage.splice(imageIndex, 1)
-                            //     console.log(previousCartImage.length)
-                            //     console.log(previousCartImage)
-                                
-                            // }
+                            let imageIndex = previousCartImage.indexOf(removedId)
+                            if (imageIndex !== -1) {
+                                previousCartImage.splice(imageIndex, 1)
+                                numberofCart.innerHTML = previousCartImage.length;
+                                numberOfitem.innerHTML = previousCartImage.length;
+                            }
                             
                             let currentSubTotal2 = parseInt(currentSubTotal.replace("$", ""));
                             let newTotal = currentSubTotal2 -= removeTotalPrice2 ;
@@ -318,7 +332,8 @@ function AddItemToCart(placeholder){
 }
 
 //should redirect to the details page when you on the product image
-let pict = ""
+
+
 let currentImag = 0
 let otherParts;
 let mainPart;
@@ -327,27 +342,40 @@ function Redirect(placeholder) {
     placeholder.forEach(proDucts => {
        
         proDucts.addEventListener("click", (e) => {
-            const mainBody=document.querySelector(".bodyMain")
+            let pict = ""
+
             homepage.style.display = "none"
             collection.style.display = "none"
             mainBody.style.display = "flex"
             
             let Target = e.target;
             let product = Target.parentElement.parentElement.id;
-            
+            let productName = document.querySelector(".dis1");
+            let productPrice=document.querySelector(".priceI")
+            const addItemToCart = document.querySelector('.Cartbutton');
+            addItemToCart.addEventListener("click", () => {
+                alert("okay")
+            })
+
+
             for (let i = 0; i < products.length; i++){
                 if (products[i].id === product) {
                     otherParts = products[i].otherPart
-                    mainPart=products[i].picture
+                    mainPart = products[i].picture
+                    productName.innerHTML = products[i].name
+                    productPrice.innerHTML = products[i].price
                 }
             }
             for (let i = 0; i < 4; i++){
-               pict += ` <div>
+              pict += ` <div>
                         <img src=${otherParts[i]}>
                      </div>`
                 document.querySelector(".others").innerHTML=pict
             }
             let displayImage = document.querySelector(".displayed");
+            
+
+           
             displayImage.innerHTML=`<img src=${otherParts[currentImag]}>`
             console.log(mainPart)
 
@@ -373,12 +401,13 @@ function NextBackBtn() {
         let backBtn = document.querySelector(".previous")
         backBtn.addEventListener("click", () => {
             currentImag -= 1;
-            if (currentImag === 0) {
-                currentImag=4
+            if (currentImag === -1) {
+                currentImag=3
             }
+            console.log(currentImag)
             let displayImage = document.querySelector(".displayed");
             displayImage.innerHTML=`<img src=${otherParts[currentImag]}>`
-            console.log(currentImag)
+            
         })
     }
     Back()
